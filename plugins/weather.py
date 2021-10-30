34596c3e00c188a88a7dc48e3ca7d598b2b2790f
Damian Demiraj <damian.demiraj@buhlergroup.com>
2021-10-30 13:34:53 +0200

    weather.py update

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,14 +1,6 @@
 import requests
 
 def run(value):
-    weather = requests.get('https://api.openweathermap.org/data/2.5/weather?q=St.%20Gallen&appid=92b6b47a3146d720b6bb622ce7f1583c')
-    json = weather.json()
-    output = """
-        {
-            'name' :""" + json["name"]        + """
-            'temp' :""" + json["temp"]-273.15 + """
-            'speed':""" + json["speed"]       + """
-        }
-    """
-    
-    output += ','
+    request = requests.get('https://api.openweathermap.org/data/2.5/weather?q=St.%20Gallen&appid=92b6b47a3146d720b6bb622ce7f1583c')
+    json = request.json()
+    return json[value]
