6dd7619d95b56daa20f1ab6a5b37e301cdf22dbf
Damian Demiraj <damian.demiraj@buhlergroup.com>
2021-10-30 15:14:05 +0200

    Additions to wheather

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,6 +1,13 @@
+from os import name
 import requests
+import json
 
 def run(value):
     request = requests.get('https://api.openweathermap.org/data/2.5/weather?q=St.%20Gallen&appid=92b6b47a3146d720b6bb622ce7f1583c')
-    json = request.json()
-    return json[value]
+    if value == 'name':
+        return request.json()['name']
+    if value == 'speed':
+        return request.json()['wind']['speed']
+    if value == 'temp':
+        r = request.json()['main']['temp']
+        return r-273,15
