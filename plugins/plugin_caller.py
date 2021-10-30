95199f207e1461da51f8e7054e28f4d969559fad
Damian Demiraj <damian.demiraj@buhlergroup.com>
2021-10-30 06:22:58 +0200

    comments

--- plugins/plugin_caller.py
+++ plugins/plugin_caller.py
@@ -19,24 +19,30 @@ def call_plugin(id, value = None):
     if id == 0:
         error_handler.create_error_log('Button id not found')
         return 0
+    # Check the custom plugin folder
     try:
         custom_plugin_folder = os.path.abspath(config.plugin_dir + '/' + str(action["script"]))
         spec   = importlib.util.spec_from_file_location(action["script"], custom_plugin_folder)
         plugin = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(plugin)
+    # If the file does not exist in the custom plugin folder check the standart plugin folder
     except:
-        standart_plugin_folder = os.path.abspath('plugins/' + str(action["script"]))
-        spec   = importlib.util.spec_from_file_location(action["script"], standart_plugin_folder)
-        plugin = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(plugin)
+        try:
+            standart_plugin_folder = os.path.abspath('plugins/' + str(action["script"]))
+            spec   = importlib.util.spec_from_file_location(action["script"], standart_plugin_folder)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+        except:
+            error_handler.create_error_log("Wrong plugin name")
     finally:
+        # Execute the code with different methods depending on the input
         if "async" in action and action["async"] == True:
             asyncThread = threading.Thread(target=plugin.run)
             try:
                 if "params" in action:
-                    asyncThread.start(value, action["params"], 'ab')
+                    asyncThread.start(value, action["params"])
                 else:
-                    asyncThread.start(value, 'ab')
+                    asyncThread.start(value)
             except:
                 error_handler.create_error_log('Error in plugin execution')
         else:
