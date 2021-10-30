11eb3b82357faf25cf92f78a83252d40e25ecc11
bitcubix <bitcubix@bitcubix.io>
2021-10-30 02:05:46 +0200

    add open_website plugin remove old version

--- plugins/start_page_test.py
+++
@@ -1,9 +0,0 @@
-# import unittest
-# import start_page
-
-# class TestStringMethods(unittest.TestCase):
-
-#     def test_upper(self):
-#         self.assertEqual(start_page.test01(), 5)
-
-# unittest.main()
